d007d41d35e3e9d755b2cb2e4638a8d7c07dfd51
lexus210 <73870803@continental.edu.pe>
2024-09-06 17:31:53 -0500

    Se agrega codigo para dibujar un cuadrado

---
+++ main.py
@@ -0,0 +1,6 @@
+# Solicitar el tamaño del cuadrado al usuario
+tamaño = int(input("Introduce el tamaño del cuadrado: "))
+
+# Dibujar el cuadrado
+for i in range(tamaño):
+    print('*' * tamaño)  # Imprimir una línea de asteriscos del tamaño especificado
