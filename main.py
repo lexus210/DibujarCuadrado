16249aa464320d16661ed40c22c240889d6bd230
lexus210 <73870803@continental.edu.pe>
2024-09-06 17:41:23 -0500

    Se modifico el codigo para que sea POO

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+class Cuadrado:
+    def __init__(self, tamaño):
+        self.tamaño = tamaño
+
+    def dibujar(self):
+        for _ in range(self.tamaño):
+            print('*' * self.tamaño)
+
 # Solicitar el tamaño del cuadrado al usuario
 tamaño = int(input("Introduce el tamaño del cuadrado: "))
 
+# Crear una instancia de la clase Cuadrado
+cuadrado = Cuadrado(tamaño)
+
 # Dibujar el cuadrado
-for i in range(tamaño):
-    print('*' * tamaño)  # Imprimir una línea de asteriscos del tamaño especificado
+cuadrado.dibujar()
